a8daa28c07b5434e640675d6c7d46efc4015e6d1
Siddhesh Poyarekar <siddhesh@gotplt.org>
2021-12-17 16:49:22 +0530

    tree-object-size: Support dynamic sizes in conditions
    Handle GIMPLE_PHI and conditionals specially for dynamic objects,
    returning PHI/conditional expressions instead of just a MIN/MAX
    estimate.
    
    This makes the returned object size variable for loops and conditionals,
    so tests need to be adjusted to look for precise size in some cases.
    builtin-dynamic-object-size-5.c had to be modified to only look for
    success in maximum object size case and skip over the minimum object
    size tests because the result is no longer a compile time constant.
    
    I also added some simple tests to exercise conditionals with dynamic
    object sizes.
    
    gcc/ChangeLog:
    
    	* builtins.c (fold_builtin_object_size): Adjust for dynamic size
    	expressions.
    	* tree-object-size.c: Include gimplify-me.h.
    	(struct object_size_info): New member UNKNOWNS.
    	(size_initval_p, size_usable_p, object_sizes_get_raw): New
    	functions.
    	(object_sizes_get): Return suitable gimple variable for
    	object size.
    	(bundle_sizes): New function.
    	(object_sizes_set): Use it and handle dynamic object size
    	expressions.
    	(object_sizes_set_temp): New function.
    	(size_for_offset): Adjust for dynamic size expressions.
    	(emit_phi_nodes, propagate_unknowns, gimplify_size_expressions):
    	New functions.
    	(compute_builtin_object_size): Call gimplify_size_expressions
    	for OST_DYNAMIC.
    	(dynamic_object_size): New function.
    	(cond_expr_object_size): Use it.
    	(phi_dynamic_object_size): New function.
    	(collect_object_sizes_for): Call it for OST_DYNAMIC.  Adjust to
    	accommodate dynamic object sizes.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.dg/builtin-dynamic-object-size-0.c: New tests.
    	* gcc.dg/builtin-dynamic-object-size-10.c: Add comment.
    	* gcc.dg/builtin-dynamic-object-size-5-main.c: New file.
    	* gcc.dg/builtin-dynamic-object-size-5.c: Use it and change test
    	to dg-do run.
    	* gcc.dg/builtin-object-size-5.c [!N]: Define N.
    	(test1, test2, test3, test4) [__builtin_object_size]: Expect
    	exact result for __builtin_dynamic_object_size.
    	* gcc.dg/builtin-object-size-1.c [__builtin_object_size]: Expect
    	exact size expressions for __builtin_dynamic_object_size.
    	* gcc.dg/builtin-object-size-2.c [__builtin_object_size]:
    	Likewise.
    	* gcc.dg/builtin-object-size-3.c [__builtin_object_size]:
    	Likewise.
    	* gcc.dg/builtin-object-size-4.c [__builtin_object_size]:
    	Likewise.
    	* gcc.dg/builtin-object-size-5.c [__builtin_object_size]:
    	Likewise.
    
    Signed-off-by: Siddhesh Poyarekar <siddhesh@gotplt.org>

---
+++ gcc/testsuite/gcc.dg/builtin-dynamic-object-size-0.c
@@ -0,0 +1,77 @@
+/* { dg-do run } */
+/* { dg-options "-O2" } */
+
+typedef __SIZE_TYPE__ size_t;
+#define abort __builtin_abort
+
+size_t
+__attribute__ ((noinline))
+test_builtin_malloc_condphi (int cond)
+{
+  void *ch;
+ 
+  if (cond)
+    ch = __builtin_malloc (32);
+  else
+    ch = __builtin_malloc (64);
+
+  size_t ret = __builtin_dynamic_object_size (ch, 0);
+
+  __builtin_free (ch);
+  return ret;
+}
+
+size_t
+__attribute__ ((noinline))
+test_builtin_calloc_condphi (size_t cnt, size_t sz, int cond)
+{
+  struct
+    {
+      int a;
+      char b;
+    } bin[cnt];
+
+  char *ch = __builtin_calloc (cnt, sz);
+  size_t ret = __builtin_dynamic_object_size (cond ? ch : (void *) &bin, 0);
+
+  __builtin_free (ch);
+  return ret;
+}
+
+size_t
+__attribute__ ((noinline))
+test_deploop (size_t sz, size_t cond)
+{
+  char *bin = __builtin_alloca (32);
+
+  for (size_t i = 0; i < sz; i++)
+    if (i == cond)
+      bin = __builtin_alloca (64);
+
+  return __builtin_dynamic_object_size (bin, 0);
+}
+
+unsigned nfails = 0;
+
+#define FAIL() ({ \
+  __builtin_printf ("Failure at line: %d\n", __LINE__);			      \
+  nfails++;								      \
+})
+
+int
+main (int argc, char **argv)
+{
+  if (test_builtin_malloc_condphi (1) != 32)
+    FAIL ();
+  if (test_builtin_malloc_condphi (0) != 64)
+    FAIL ();
+  if (test_builtin_calloc_condphi (128, 1, 0) == 128)
+    FAIL ();
+  if (test_deploop (128, 129) != 32)
+    FAIL ();
+
+  if (nfails > 0)
+    __builtin_abort ();
+
+  return 0;
+}
