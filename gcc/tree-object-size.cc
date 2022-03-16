f1d8cdfe151676df135fd4317df7f330302df504
Siddhesh Poyarekar <siddhesh@gotplt.org>
2022-03-16 17:56:08 +0530

    tree-optimization/104941: Actually assign the conversion result
    Assign the result of fold_convert to offset.
    
    gcc/ChangeLog:
    
    	PR tree-optimization/104941
    	* tree-object-size.cc (size_for_offset): Assign result of
    	fold_convert to OFFSET.
    
    gcc/testsuite/ChangeLog:
    
    	PR tree-optimization/104941
    	* gcc.dg/builtin-dynamic-object-size-0.c (S1, S2): New structs.
    	(test_alloc_nested_structs, g): New functions.
    	(main): Call test_alloc_nested_structs.
    
    Signed-off-by: Siddhesh Poyarekar <siddhesh@gotplt.org>

--- gcc/tree-object-size.cc
+++ gcc/tree-object-size.cc
@@ -372,7 +372,7 @@ size_for_offset (tree sz, tree offset, tree wholesize = NULL_TREE)
 
   /* Safe to convert now, since a valid net offset should be non-negative.  */
   if (!types_compatible_p (TREE_TYPE (offset), sizetype))
-    fold_convert (sizetype, offset);
+    offset = fold_convert (sizetype, offset);
 
   if (TREE_CODE (offset) == INTEGER_CST)
     {
